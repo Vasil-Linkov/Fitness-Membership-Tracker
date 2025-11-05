73b2d69ec02f6d20614bb9eb311cd1e98112df47
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2025-11-05 15:00:21 +0200

    Add project files.

---
+++ Fitness-Membership-Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fitness_Membership_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
