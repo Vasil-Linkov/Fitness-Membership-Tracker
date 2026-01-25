8165ec38be83c4249805d102afd351fa7f05f7d2
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-01-25 18:15:21 +0200

    updated logic in logout due to previous oversight

--- Fitness-Membership-Tracker/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Fitness-Membership-Tracker/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +15,10 @@ namespace Fitness_Membership_Tracker.Web.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<Member> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<Member> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
