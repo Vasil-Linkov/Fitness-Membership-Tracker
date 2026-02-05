d5d0d440daa4de781a97373918dd79a515f96a32
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-02-05 11:18:12 +0200

    Model for displaying membership tiers

---
+++ Fitness-Membership-Tracker/Models/BuyNewMembershipViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Models
+{
+	public class BuyNewMembershipViewModel
+	{
+
+		public List<MembershipTier> MembershipTiers { get; set; } = new List<MembershipTier>();
+	}
+}
