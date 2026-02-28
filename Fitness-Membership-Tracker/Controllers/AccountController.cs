a081c57b3ef16fbb822ded734c7ec23c7005e1af
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-02-28 12:58:49 +0200

    create classes for custom login and register

---
+++ Fitness-Membership-Tracker/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Fitness_Membership_Tracker.Data.Data.DataModels;
+using Fitness_Membership_Tracker.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness_Membership_Tracker.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<Member> _signInManager;
+        private readonly UserManager<Member> _userManager;
+
+        public AccountController(
+            SignInManager<Member> signInManager,
+            UserManager<Member> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+
+            if (user.IsDeleted)
+            {
+                ModelState.AddModelError("", "Account is deactivated.");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                user,
+                model.Password,
+                model.RememberMe,
+                lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Fitness-Membership-Tracker/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fitness_Membership_Tracker.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        public bool RememberMe { get; set; }
+    }
+}
