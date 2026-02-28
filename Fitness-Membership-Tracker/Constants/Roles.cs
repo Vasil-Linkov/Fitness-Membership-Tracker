dc0bea213c3d0c351a27e364bc53f66a6a958bcd
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-02-28 12:57:56 +0200

    move roles to a constant class

---
+++ Fitness-Membership-Tracker/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Fitness_Membership_Tracker.Constants
+{
+    public class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Employee = "Employee";
+        public const string Member = "Member";
+    }
+}
