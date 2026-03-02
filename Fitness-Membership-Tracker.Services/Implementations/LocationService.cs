76cea6370861f22a938d7eb10fc2abf4e08c534a
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-02 12:41:16 +0200

    Add location service

---
+++ Fitness-Membership-Tracker.Services/Implementations/LocationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+using Fitness_Membership_Tracker.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Membership_Tracker.Services.Implementations
+{
+    public class LocationService : ILocationService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Location>> GetAllAsync()
+        {
+            return await _context.Locations.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Location?> GetByIdAsync(int id)
+        {
+            return _context.Locations.AsNoTracking().FirstOrDefaultAsync(l => l.Id == id);
+        }
+    }
+}
---
+++ Fitness-Membership-Tracker.Services/Interfaces/ILocationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<List<Location>> GetAllAsync();
+        Task<Location?> GetByIdAsync(int id);
+    }
+}
