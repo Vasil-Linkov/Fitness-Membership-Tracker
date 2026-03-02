f4e84efde46f45f7d11309afa3259ef62f2f6218
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-02 12:37:25 +0200

    Add payment service

---
+++ Fitness-Membership-Tracker.Services/Implementations/PaymentService.cs
@@ -0,0 +1,40 @@
+﻿using Fitness_Membership_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+using Fitness_Membership_Tracker.Services.Interfaces;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Implementations;
+
+public class PaymentService : IPaymentService
+{
+    private readonly ApplicationDbContext _context;
+
+    public PaymentService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Payment>> GetAllAsync()
+    {
+        return await _context.Payments
+            .Include(p => p.Member)
+            .Include(p => p.Membership)
+            .ToListAsync();
+    }
+
+    public async Task<List<Payment>> GetByMemberIdAsync(string memberId)
+    {
+        return await _context.Payments
+            .Where(p => p.MemberId == memberId)
+            .Include(p => p.Membership)
+            .ToListAsync();
+    }
+
+    public async Task CreateAsync(Payment payment)
+    {
+        payment.PaymentDate = DateTime.UtcNow;
+
+        _context.Payments.Add(payment);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Fitness-Membership-Tracker.Services/Interfaces/IPaymebtService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<List<Payment>> GetAllAsync();
+        Task<List<Payment>> GetByMemberIdAsync(string memberId);
+        Task CreateAsync(Payment payment);
+    }
+}
