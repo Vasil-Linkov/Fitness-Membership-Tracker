4ae2a679a8200bd6ef24a133ade1e07d06ad0a7a
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-02 12:53:46 +0200

    Add Member service

---
+++ Fitness-Membership-Tracker.Services/Implementations/MemberService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+using Fitness_Membership_Tracker.Data;
+using Fitness_Membership_Tracker.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Membership_Tracker.Services.Implementations
+{
+    public class MemberService : IMemberService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MemberService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Member>> GetAllAsync()
+            => await _context.Users
+                .Include(m => m.Membership)
+                .AsNoTracking()
+                .ToListAsync();
+
+        public async Task<Member?> GetByIdAsync(string id)
+            => await _context.Users
+                .Include(m => m.Membership)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+        public async Task UpdateAsync(Member member)
+        {
+            _context.Users.Update(member);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var member = await GetByIdAsync(id);
+            
+            if (member == null) return;
+
+            member.IsDeleted = true;
+
+            _context.Users.Update(member);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Fitness-Membership-Tracker.Services/Interfaces/IMemberService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Interfaces
+{
+    public interface IMemberService
+    {
+        Task<List<Member>> GetAllAsync();
+        Task<Member?> GetByIdAsync(string id);
+        Task UpdateAsync(Member member);
+        Task DeleteAsync(string id);
+    }
+}
