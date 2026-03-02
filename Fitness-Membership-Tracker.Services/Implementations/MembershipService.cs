4fc272e103f64b0fc52b9bacadc42acc4a911638
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-02 13:08:50 +0200

    Add membership service

---
+++ Fitness-Membership-Tracker.Services/Implementations/MembershipService.cs
@@ -0,0 +1,54 @@
+﻿using Fitness_Membership_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+using Fitness_Membership_Tracker.Services.Interfaces;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Implementations;
+
+public class MembershipService : IMembershipService
+{
+    private readonly ApplicationDbContext _context;
+
+    public MembershipService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Membership>> GetAllAsync()
+    {
+        return await _context.Memberships.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Membership?> GetByIdAsync(int id)
+    {
+        return await _context.Memberships.AsNoTracking().FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task CreateAsync(Membership membership)
+    {
+        _context.Memberships.Add(membership);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Membership updatedMembership)
+    {
+        var oldMembership = await GetByIdAsync(updatedMembership.Id);
+        if (oldMembership == null) return;
+
+        oldMembership = updatedMembership;
+
+        _context.Memberships.Update(oldMembership);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var membership = await GetByIdAsync(id);
+        if (membership == null) return;
+
+        membership.IsDeleted = true;
+
+        _context.Memberships.Update(membership);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Fitness-Membership-Tracker.Services/Interfaces/IMembershipService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Interfaces
+{
+    public interface IMembershipService
+    {
+        Task<List<Membership>> GetAllAsync();
+        Task<Membership?> GetByIdAsync(int id);
+        Task CreateAsync(Membership membership);
+        Task UpdateAsync(Membership membership);
+        Task DeleteAsync(int id);
+    }
+}
