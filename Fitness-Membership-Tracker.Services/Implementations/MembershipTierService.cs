0f13ab02f0624dee1651386d919c171bdc96251a
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-06 09:28:48 +0200

    Add and implement MembershipTierService

---
+++ Fitness-Membership-Tracker.Services/Implementations/MembershipTierService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data;
+using Fitness_Membership_Tracker.Data.DataModels;
+using Fitness_Membership_Tracker.Services.Interfaces;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Membership_Tracker.Services.Implementations
+{
+    public class MembershipTierService : IMembershipTierService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembershipTierService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MembershipTier>> GetTiersAsync()
+            => await _context.MembershipTiers.AsNoTracking().ToListAsync();
+    }
+}
---
+++ Fitness-Membership-Tracker.Services/Interfaces/IMembershipTierService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness_Membership_Tracker.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Services.Interfaces
+{
+    public interface IMembershipTierService
+    {
+
+        public Task<List<MembershipTier>> GetTiersAsync();
+    }
+}
