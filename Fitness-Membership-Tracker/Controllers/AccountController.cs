45fcbf50034707ec788328bc0d768054e2e307d4
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-08 13:33:58 +0200

    implement register

--- Fitness-Membership-Tracker/Controllers/AccountController.cs
+++ Fitness-Membership-Tracker/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Fitness_Membership_Tracker.Data.Data.DataModels;
 using Fitness_Membership_Tracker.Data.DataModels;
+using Fitness_Membership_Tracker.Models;
 using Fitness_Membership_Tracker.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -66,6 +67,48 @@ namespace Fitness_Membership_Tracker.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+			if (!ModelState.IsValid)
+				return View(model);
+
+			var existingUserByEmail = await _userManager.FindByEmailAsync(model.Email);
+			if (existingUserByEmail != null)
+			{
+				ModelState.AddModelError(nameof(model.Email), "Email is already in use.");
+				return View(model);
+			}
+
+
+			var user = new Member
+            {
+				UserName = model.Email,
+				Email = model.Email,
+				IsDeleted = false
+			};
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (result.Succeeded)
+            {
+				await _signInManager.SignInAsync(user, isPersistent: false);
+				return RedirectToAction("Index", "Home");
+			}
+
+			foreach (var error in result.Errors)
+				ModelState.AddModelError("", error.Description);
+
+			return View(model);
         }
 
     }
