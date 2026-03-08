35ae1efd8a97e6fb0161769546abe8a45f3a4141
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-08 16:06:34 +0200

    Add case logic to purchasing a membership and pilished membership controller

--- Fitness-Membership-Tracker/Controllers/MembershipController.cs
+++ Fitness-Membership-Tracker/Controllers/MembershipController.cs
@@ -1,122 +1,149 @@
-﻿using Fitness_Membership_Tracker.Data;
-
-using Fitness_Membership_Tracker.Data.DataModels;
+﻿using Fitness_Membership_Tracker.Data.DataModels;
 using Fitness_Membership_Tracker.Models;
 using Fitness_Membership_Tracker.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Fitness_Membership_Tracker.Controllers
 {
-    public class MembershipController : Controller
-    {
+	[Authorize]
+	public class MembershipController : Controller
+	{
 		private readonly IMemberService _memberService;
 		private readonly IMembershipService _membershipService;
-        private readonly IMembershipTierService _membershipTierService;
-        private readonly ILocationService _locationService;
-        private readonly IEmployeeService _employeeService;
-        private readonly IPaymentService _paymentService;
+		private readonly IMembershipTierService _membershipTierService;
+		private readonly ILocationService _locationService;
+		private readonly IEmployeeService _employeeService;
+		private readonly IPaymentService _paymentService;
 
 		public MembershipController(
-            IMembershipService membershipService,
-            IMemberService memberService,
-            IMembershipTierService membershipTierService,
-            ILocationService locationService,
-            IEmployeeService employeeService,
-            IPaymentService paymentService)
-        {
-            _membershipService = membershipService;
-            _memberService = memberService;
-            _membershipTierService = membershipTierService;
-            _locationService = locationService;
-            _employeeService = employeeService;
-            _paymentService = paymentService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> YourMembership()
-        {
-            var user = await _memberService.GetByNameAsync(User.Identity.Name);
-
-            var membershipViewModel = await _membershipService.GetMembershipByMember(user);
-            
-
-            if (membershipViewModel == null)
-            {
-                return View(null);
-            }
-
-            return View(membershipViewModel);
-        }
-
-        public async Task<IActionResult> BuyNewMembership()
-        {
-            var membershipTiers = new BuyNewMembershipViewModel()
-            {
-                MembershipTiers = await _membershipTierService.GetTiersAsync()
-			};
-
-			return View(membershipTiers);
+			IMembershipService membershipService,
+			IMemberService memberService,
+			IMembershipTierService membershipTierService,
+			ILocationService locationService,
+			IEmployeeService employeeService,
+			IPaymentService paymentService)
+		{
+			_membershipService = membershipService;
+			_memberService = memberService;
+			_membershipTierService = membershipTierService;
+			_locationService = locationService;
+			_employeeService = employeeService;
+			_paymentService = paymentService;
 		}
 
-        [HttpPost]
-        public async Task<IActionResult> PurchaseMembership(int membershipTierId)
-        {
-            var membershipTier = await _membershipTierService.GetByIdAsync(membershipTierId);
+		[HttpGet]
+		public async Task<IActionResult> YourMembership()
+		{
+			var member = await _memberService.GetByNameAsync(User.Identity.Name);
 
-            if (membershipTier != null && User.Identity != null && User.Identity.IsAuthenticated)
-            {
-                var member = await _memberService.GetByIdAsync(User.Identity.Name);
+			if (member == null)
+			{
+				return View(new YourMembershipViewModel());
+			}
 
-				var locations = await _locationService.GetAllAsync();
+			var membership = await _membershipService.GetMembershipByMember(member);
 
-                var location = locations[Random.Shared.Next(0, locations.Count)];
+			var model = new YourMembershipViewModel
+			{
+				Membership = membership
+			};
 
-                var employeesAtLocation = await _employeeService.GetEmployeesAsync(location.Id, string.Empty);
+			return View(model);
+		}
 
-                Employee employee = null;
+		[HttpGet]
+		public async Task<IActionResult> BuyNewMembership()
+		{
+			var model = new BuyNewMembershipViewModel
+			{
+				MembershipTiers = await _membershipTierService.GetTiersAsync()
+			};
 
-                if (employeesAtLocation.Any())
-                {
-                    employee = employeesAtLocation[
-                        Random.Shared.Next(0, employeesAtLocation.Count)
-                    ];
-                }
+			return View(model);
+		}
 
-				var newMembership = new Membership()
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> PurchaseMembership(int membershipTierId)
+		{
+			var membershipTier = await _membershipTierService.GetByIdAsync(membershipTierId);
+			var member = await _memberService.GetByNameAsync(User.Identity.Name);
+			var locations = await _locationService.GetAllAsync();
+			var location = locations[Random.Shared.Next(locations.Count)];
+
+			var employeesAtLocation = await _employeeService.GetEmployeesAsync(location.Id, string.Empty);
+
+			Employee? employee = null;
+			if (employeesAtLocation.Any())
+			{
+				employee = employeesAtLocation[Random.Shared.Next(employeesAtLocation.Count)];
+			}
+
+			Membership membership;
+
+			int? membershipId = member.MembershipId;
+
+			Membership? existingMembership;
+
+			if (membershipId == null)
+				existingMembership = null;
+			else
+				existingMembership = await _membershipService.GetByIdIncludingDeletedAsync(membershipId.Value);
+
+
+			if (existingMembership == null)
+			{
+				membership = new Membership
 				{
 					MembershipTierId = membershipTier.Id,
 					StartDate = DateTime.Now,
 					EndDate = DateTime.Now.AddMonths(1),
-					LocationId = location.Id
+					LocationId = location.Id,
+					IsDeleted = false,
+					DeletedAt = null
 				};
 
-				await _membershipService.CreateAsync(newMembership);
+				await _membershipService.CreateAsync(membership);
 
-                var payment = new Payment()
-                {
-                    Currency = "EUR",
-                    Amount = membershipTier.MonthlyPrice,
-                    PaymentDate = DateTime.Now,
-                    PaymentMethod = "OnSite",
-                    MemberId = member.Id,
-                    MembershipId = newMembership.Id,
-                    EmployeeId = employee?.Id
-                };
+				member.MembershipId = membership.Id;
+				await _memberService.UpdateAsync(member);
+			}
+			else
+			{
+				existingMembership.MembershipTierId = membershipTier.Id;
+				existingMembership.LocationId = location.Id;
 
-                await _paymentService.CreateAsync(payment);
+				if (existingMembership.IsDeleted || existingMembership.EndDate < DateTime.Now)
+				{
+					existingMembership.IsDeleted = false;
+					existingMembership.DeletedAt = null;
+					existingMembership.StartDate = DateTime.Now;
+					existingMembership.EndDate = DateTime.Now.AddMonths(1);
+				}
+				else
+				{
+					existingMembership.EndDate = existingMembership.EndDate.AddMonths(1);
+				}
 
-                member.MembershipId = newMembership.Id;
-                await _memberService.UpdateAsync(member);
-                
-            }
-            else
-            {
-                throw new Exception("Membership Tier not found or user not authenticated.");
-            }
+				await _membershipService.UpdateAsync(existingMembership);
+				membership = existingMembership;
+			}
 
-            return Redirect("YourMembership");
-        }
+			var payment = new Payment
+			{
+				Currency = "EUR",
+				Amount = membershipTier.MonthlyPrice,
+				PaymentDate = DateTime.Now,
+				PaymentMethod = "OnSite",
+				MemberId = member.Id,
+				MembershipId = membership.Id,
+				EmployeeId = employee?.Id
+			};
 
-    }
+			await _paymentService.CreateAsync(payment);
+
+			return RedirectToAction(nameof(YourMembership));
+		}
+	}
 }
