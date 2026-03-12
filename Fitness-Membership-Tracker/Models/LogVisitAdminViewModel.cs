b3b8a8c05a3f77a353e0821f42e84b0a7daa02e3
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-12 12:19:48 +0200

    Add ViewModels for visit views

---
+++ Fitness-Membership-Tracker/Models/LogVisitAdminViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Fitness_Membership_Tracker.Models
+{
+    public class LogVisitAdminViewModel
+    {
+        [Required]
+        public string MemberId { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+        public int? MembershipId { get; set; }
+
+        public IEnumerable<SelectListItem> Members { get; set; } = Enumerable.Empty<SelectListItem>();
+        public IEnumerable<SelectListItem> Locations { get; set; } = Enumerable.Empty<SelectListItem>();
+        public IEnumerable<SelectListItem> Memberships { get; set; } = Enumerable.Empty<SelectListItem>();
+    }
+}
---
+++ Fitness-Membership-Tracker/Models/VisitStatsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Fitness_Membership_Tracker.Data.DataModels;
+
+namespace Fitness_Membership_Tracker.Models
+{
+    public class VisitStatsViewModel
+    {
+        public Dictionary<DateTime, int> DailyVisitCounts { get; set; } = new();
+
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public int TotalVisits { get; set; }
+        public double AveragePerDay { get; set; }
+        public int PeakCount { get; set; }
+        public DateTime? PeakDay { get; set; }
+
+        public List<Visit>? Visits { get; set; }
+    }
+}
