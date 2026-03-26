be5d036ce4f9d15c65c64c01e4333a344019374a
VasilLinkov <128574324+VasilLinkov@users.noreply.github.com>
2026-03-26 10:16:57 +0200

    Seed data in runtime

--- Fitness-Membership-Tracker/Program.cs
+++ Fitness-Membership-Tracker/Program.cs
@@ -1,12 +1,10 @@
 using Fitness_Membership_Tracker.Data;
-using Fitness_Membership_Tracker;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Fitness_Membership_Tracker.Services.Implementations;
 using Fitness_Membership_Tracker.Services.Interfaces;
 using Fitness_Membership_Tracker.Data.DataModels;
-using Fitness_Membership_Tracker.Services;
+using Fitness_Membership_Tracker.Data.Seeding;
 
 namespace Fitness_Membership_Tracker
 {
@@ -16,18 +14,20 @@ namespace Fitness_Membership_Tracker
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            // ?? Database ?????????????????????????????????????????????????????
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
-            
+            // ?? Identity ?????????????????????????????????????????????????????
             builder.Services.AddIdentity<Member, IdentityRole>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
                 options.SignIn.RequireConfirmedEmail = true;
 
-                options.Lockout.AllowedForNewUsers = false; 
+                options.Lockout.AllowedForNewUsers = false;
                 options.Lockout.MaxFailedAccessAttempts = int.MaxValue;
 
                 options.Password.RequireDigit = true;
@@ -36,11 +36,13 @@ namespace Fitness_Membership_Tracker
                 options.Password.RequireUppercase = true;
                 options.Password.RequireLowercase = true;
             })
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
 
+            // ?? MVC ??????????????????????????????????????????????????????????
             builder.Services.AddControllersWithViews();
 
+            // ?? Application services ?????????????????????????????????????????
             builder.Services.AddScoped<IEmployeeService, EmployeeService>();
             builder.Services.AddScoped<IMemberService, MemberService>();
             builder.Services.AddScoped<IMembershipService, MembershipService>();
@@ -54,39 +56,35 @@ namespace Fitness_Membership_Tracker
             builder.Services.AddScoped<ITrainingRequestService, TrainingRequestService>();
             builder.Services.AddScoped<IWorkoutService, WorkoutService>();
 
-
-
+            // ????????????? Build app ?????????????????????????????????????????????????
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
+            // ?? HTTP pipeline ?????????????????????????????????????????????????
+            if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                await DBSeeding.SeedRolesAndAdminAsync(services);
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            // ?? Seed database on startup ??????????????????????????????????????
+            using (var scope = app.Services.CreateScope())
+            {
+                await DataSeeder.SeedAllAsync(scope.ServiceProvider);
+            }
 
             app.Run();
         }
